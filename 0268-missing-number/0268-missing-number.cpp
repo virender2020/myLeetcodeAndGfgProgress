5923c0cbd0aa7b5128295a1de64ea85af136ecb6
Courage <95067927+virender2020@users.noreply.github.com>
2023-02-22 03:45:49 +0530

    Time: 24 ms (50.75%), Space: 18.5 MB (9.64%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& A) {
+         vector<int>frequency(A.size()+1,0);
+    int a;
+    for(int i=0;i<A.size();i++){
+        frequency[A[i]]++;
+    }
+    for(int i=0;i<=A.size();i++){
+         if(frequency[i]==0)
+        a=i;
+    }
+    return a;
+    }
+};
